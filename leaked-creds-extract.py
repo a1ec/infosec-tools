ac0d0df14012817a54ab457ba2e312e530da3657
root <acsus2@gmail.com>
2016-12-12 19:14:00 -0500

    Changed creds output to comma separated

--- leaked-creds-extract.py
+++ leaked-creds-extract.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.5
 # AC - 2016-11-02
-# extracts credentials from saved leaked source html files
+# extracts email credentials from saved leaked source html files
 
 from lxml import html, etree
 import sys
@@ -16,13 +16,15 @@ def extract_leaked_source_email_creds(filename):
     parser = etree.HTMLParser()
     tree = etree.parse(filename, parser)
 
+    # iterate through the right nodes and extract the credentials
     for credential_tables in tree.xpath("/html/body/center/center/table/tbody"):
         email = credential_tables.xpath("tr/td[contains(text(), 'email: ')]/text()")
         password = credential_tables.xpath("tr/td[contains(text(), 'Real_Password: ')]/text()")
-        # print email:password pair to stdout
-        # TODO find out where those newlines are coming from, I didn't put them
-        sys.stdout.write("{} {}".format(extract_credential(email, 'email: '), \
+        
+	# print comma separated email and password pair to stdout
+        sys.stdout.write("{},{}".format(extract_credential(email, 'email: '), \
                              extract_credential(password, 'Real_Password: ')))
+        # TODO find where those first empty lines are coming from
 
 
 # extract creds from each file indicated on the command line
