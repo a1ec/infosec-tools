0f2bd2c0e2a987fdbc10919be376e9a6afe16715
AC <acsus2@gmail.com>
2017-02-01 23:09:55 +1100

    Begin separation of tool invocation, tool output processing and results merging

--- domain-enum.sh
+++ domain-enum.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# enum-domain.sh - enumerate a domain's hosts and emails and generate CSV files
+# enum-domain.sh - attempts to enumerate a domain's hostnames, IPv4s and emails
 # AC - 2017-01-19
+
+# Runs the tools, filter output to simple newline separated entries, merge
 
 # check if argument passed
 if [[ ! -z "$1" ]]; then
@@ -14,32 +16,57 @@ else
 fi
 
 # run domain reconnaissance tools
-fierce -th 32 -dns $domain -wordlist /usr/share/wordlists/dnsmap.txt -file fierce.$domain
+echo " Run maltego and export CSV"; sleep 1
+echo " Run recon-ng and export CSV"; sleep 1
 theharvester -d $domain -l 1000 -b all -v theharvester.out | tee theharvester.$domain
-	
+fierce -th 32 -dns $domain -wordlist /usr/share/wordlists/dnsmap.txt -file fierce.$domain
+
 # TODO add discover - https://github.com/leebaird/discover
-# TODO add maltego
-# TODO add shodan
+# TODO add recon-ng
 # TODO add censys.io
-# TODO add altDNS
+# TODO altDNS add
 # TODO and assetnote
 
-touch $domain.hosts
-touch $domain.emails
+################
+# theharvester #
+################
+# extract hosts
+grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' theharvester.$domain | tr ':' ' ' | sed -e 's/\s\+/,/g' > theharvester.$domain.csv
+cut -d, -f1 theharvester.$domain.csv | sort -uV | tee theharvester.$domain.ipv4s
+cut -d, -f2 theharvester.$domain.csv | grep $domain | tr '[:upper:]' '[:lower:]' | sort -uV | tee theharvester.$domain.hosts
+# extract emails only from domain, remove duplicates, ignore case
+grep @$domain theharvester.$domain | sort -uf | tee theharvester.aarnet.edu.au.emails
 
-#### Email Enumeration
-# extract emails only from domain, remove duplicates
-grep '@$domain' theharvester.$domain | sort -uf >> $domain.emails
-####
+###########
+# maltego #
+###########
+# extract hostnames,currently must run maltego manually, enter domain, run machine and export to CSV
+grep $domain maltego.$domain | cut -d, -f2 | sort -uV | tee maltego.$domain.tmp
+cat maltego.$domain* | cut -d, -f1 | grep $domain | grep -v '@$domain' | tr '[:upper:]' '[:lower:]' | sort -uV | tee maltego.$domain.hosts
+grep @$domain maltego.$domain* | cut -d',' -f2 | cut -d':' -f2 | sort -uf | tee maltego.$domain.emails
 
-#### Host Enumeration
-# extract hosts from fierce output , exclude the lines reporting subnet stats
-grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' fierce.$domain | grep -v 'hostnames found' | sed -e 's/\s\+/,/g' >> $domain.hosts
+##########
+# fierce #
+##########
+grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' fierce.$domain | grep -v 'hostnames found' | sed -e 's/\s\+/,/g' | cut -d, -f1 | sort -uV | tee fierce.$domain.ipv4s
+grep $domain fierce.$domain | grep -v 'hostnames found' | sed -e 's/\s\+/,/g' | cut -d, -f2 | sort -uf | tee fierce.$domain.hosts
 
-# extract hosts from theharvester
-grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' theharvester.$domain | tr ':' ' ' | sed -e 's/\s\+/,/g' >> $domain.hosts
+############
+# recon-ng #
+############
+cut -d, -f1 recon-ng.$domain | tr -d '"' | tee recon-ng.$domain.hosts
+cut -d, -f2 recon-ng.$domain | tr -d '"' | sort -uV | tee recon-ng.$domain.ipv4s
 
+###############
+# dnsdumpster #
+###############
+# TODO curl requests for domain at https://dnsdumpster.com
+# TODO download xlsx and convert to CSV 
+cut -f1 dnsdumpster.aarnet.edu.au | sort -u | tee dnsdumpster.$domain.hosts
+cut -f2 dnsdumpster.aarnet.edu.au | sort -uV | tee dnsdumpster.$domain.ipv4s
+
+# Merge all tool output
 # fold all uppercase to lower, remove duplicates, sort hosts by IPv4 subnets and remove trailing commas
-cat $domain.hosts | tr '[:upper:]' '[:lower:]' | sort -uV | sed 's/[,.]$//' | grep $domain > $domain.hosts.tmp
-mv $domain.hosts.tmp $domain.hosts
-####
+cat *$domain.hosts  | tr '[:upper:]' '[:lower:]' | sort -uV | sed 's/[,.]$//' | grep $domain | tee $domain.hosts
+cat *$domain.emails | tr '[:upper:]' '[:lower:]' | sort -uV | sed 's/[,.]$//' | grep $domain | tee $domain.emails
+cat *$domain.ipv4s  | sort -uV  | tee $domain.ipv4s
